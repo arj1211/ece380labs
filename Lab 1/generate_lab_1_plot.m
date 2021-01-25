29d879e9617db4f6c5088f6e74d3d3020172922c
Arjun <arjun.bawa@hotmail.com>
2021-01-24 20:14:17 -0500

    done lab 1 i think

--- Lab 1/generate_lab_1_plot.m
+++ Lab 1/generate_lab_1_plot.m
@@ -1,63 +1,171 @@
-%% Run the simulink diagram!
-sim('Lab_1.slx');
-% Once the simulink diagram executes, it generates a variable "logsout". We
-% can inspect this variable to pull out the signals we have recorded!
-%% Pull out the Output (y) and Input (u) signals.
-y_t = logsout.getElement('y').Values.Time;      % Get the Time Points for the output signal 'y'
-y_v = logsout.getElement('y').Values.Data;      % Get the values for the output signal 'y'
-u_t = logsout.getElement('u').Values.Time;      % Get the time points for the input signal 'u'
-u_v = logsout.getElement('u').Values.Data;      % Get the values for the input signal 'u'
-%% Markers at Peaks
+%% 1.1
+[y_t,y_v,u_t,u_v] = run_sim('Lab_1_1.slx');
+% Estimate DC Gain
+dc_gain_open = (max(y_v)-min(y_v)) / (max(u_v)-min(u_v));
+% Markers at Peaks
 [maxy,maxind]=max(y_v);
 [miny,minind]=min(y_v);
 minx = y_t(minind);
 maxx = y_t(maxind);
 px = [minx, maxx];
 py = [miny, maxy];
-%% Estimate DC Gain
-gain = (max(y_v)-min(y_v)) / (max(u_v)-min(u_v));
-% annotation('textbox',[0.2,0.15,0.1,0.1],'String',strcat("DC Gain Estimate:",num2str(gain)));
-disp(['DC Gain Estimate: ',num2str(gain)])
-%% Predict Amplitude of Steady-State output at Bandwidth Frequency
-% Open loop B.freq = 35.6 Hz
-% Closed loop B.freq = 68 Hz
-amp_open = 0.9127/sqrt(2); % Open-loop
-amp_closed = 0.47719/sqrt(2); % Closed-loop
-disp(['Open-loop Amplitude of Steady-State output at Bandwidth Frequency: ',num2str(amp_open)])
-disp(['Closed-loop Amplitude of Steady-State output at Bandwidth Frequency: ',num2str(amp_closed)])
-%% Settling Time + Time Const. (Open-loop)
-% point_63 = 0.63*0.9128;
-% down2p = 0.9128/2*.98;
-% up2p = 0.9128/2*1.02;
-% settling_time = 0.02230355;
-% y_val = point_63 - 0.9128/2;
-% tau = 0.0048159299;
-%% Settling Time + Time Const. (Closed-loop)
-point_63 = 0.63*0.47719;
-down2p = 0.47719/2*.98;
-up2p = 0.47719/2*1.02;
-settling_time = 0.0116563755;
-y_val = point_63 - 0.47719/2;
-tau = 0.00251805496;
-%% Plot Input (u) and Output (y) Signals
-figure(1);                                      % Create a figure indexed by (1)
-plot([u_t(:), y_t(:)], [u_v(:), y_v(:)], '-');  % Plot input and output.
 
+figure(1);
+plot([u_t(:), y_t(:)], [u_v(:), y_v(:)], '-');
 hold on
-plot(px,py,'go');                               % Green circle markers
+plot(px,py,'go');
+legend('Input Signal', 'Output Signal', 'Peaks');
+xlabel('Time')
 
-% hold on
-% yline(y_val);
-% plot(1+tau,y_val,'mx');                         % 63% point (tau point)
+title('Response to Low-Frequency Input')
+annotation('textbox',[0.2,0.15,0.1,0.1],'String',strcat("DC Gain Estimate: ",num2str(dc_gain_open)));
+%% 1.2
+[y_t,y_v,u_t,u_v] = run_sim('Lab_1_2.slx');
+bwf_open = 32.5; % Hz
+% Markers at Peaks
+[maxy,maxind]=max(y_v);
+[miny,minind]=min(y_v);
+minx = y_t(minind);
+maxx = y_t(maxind);
+px = [minx, maxx];
+py = [miny, maxy];
 
-% hold on
-% yline(down2p);                                  % 2% lower
-% hold on
-% yline(up2p);                                    % 2% higher
+figure(2);
+plot([u_t(:), y_t(:)], [u_v(:), y_v(:)], '-');
+hold on
+plot(px,py,'go');
+legend('Input Signal', 'Output Signal', 'Peaks');
+xlabel('Time')
 
-% hold on
-% plot(1+settling_time, down2p, 'c+');            % settling point
+title('Time-Domain Response of Plant at Bandwidth Frequency')
+annotation('textbox',[0.2,0.15,0.1,0.1],'String',strcat("B.W. Freq.: ",num2str(bwf_open)));
+%% 1.3.1
+[y_t,y_v,u_t,u_v] = run_sim('Lab_1_3_1.slx');
+% Estimate DC Gain
+dc_gain_closed = (max(y_v)-min(y_v)) / (max(u_v)-min(u_v));
+% Markers at Peaks
+[maxy,maxind]=max(y_v);
+[miny,minind]=min(y_v);
+minx = y_t(minind);
+maxx = y_t(maxind);
+px = [minx, maxx];
+py = [miny, maxy];
 
-legend('Input Signal', 'Output Signal', 'Peaks');% Legend, in order of signals above.
-title('Put a good title here')                  % Title
-xlabel('Time')                                  % XLable is 'Time'
+figure(3);
+plot([u_t(:), y_t(:)], [u_v(:), y_v(:)], '-');
+hold on
+plot(px,py,'go');
+legend('Input Signal', 'Output Signal', 'Peaks');
+xlabel('Time')
+
+title('Closed-loop Response to Low-Frequency Input')
+annotation('textbox',[0.2,0.15,0.1,0.1],'String',strcat("DC Gain Estimate: ",num2str(dc_gain_closed)));
+%% 1.3.2
+[y_t,y_v,u_t,u_v] = run_sim('Lab_1_3_2.slx');
+% Markers at Peaks
+[maxy,maxind]=max(y_v);
+[miny,minind]=min(y_v);
+minx = y_t(minind);
+maxx = y_t(maxind);
+px = [minx, maxx];
+py = [miny, maxy];
+figure(4);
+plot([u_t(:), y_t(:)], [u_v(:), y_v(:)], '-');
+hold on
+plot(px,py,'go');
+legend('Input Signal', 'Output Signal', 'Peaks');
+xlabel('Time')
+
+title('Closed-loop Time-Domain Response of Plant at Bandwidth Frequency')
+bwf_closed = 32.5; % Hz
+annotation('textbox',[0.2,0.15,0.1,0.1],'String',strcat("B.W. Freq.: ",num2str(bwf_closed)));
+%% 1.4
+[y_t,y_v,u_t,u_v] = run_sim('Lab_1_4.slx');
+tau_y_open = dc_gain_open*0.63 - dc_gain_open/2;
+set_time_low_open = dc_gain_open/2 * 0.98;
+set_time_high_open = dc_gain_open/2 * 1.02;
+
+% From graph
+tau_open = 0.0048157462;
+settling_time_open = 0.0222780065037;
+
+figure(5);
+plot([u_t(:), y_t(:)], [u_v(:), y_v(:)], '-');
+hold on
+yline(tau_y_open,'c-');
+hold on
+yline(set_time_low_open,'g-');
+hold on
+yline(set_time_high_open,'g-');
+hold on
+plot(1+tau_open,tau_y_open,'ro');
+hold on
+plot(1+settling_time_open, set_time_low_open,'bo');
+
+legend('Input Signal', 'Output Signal');
+xlabel('Time')
+
+title('Settling Time and Time Constant for Open-loop Step-Response')
+annotation('textbox',[0.2,0.15,0.1,0.1],'String',strcat("Time Constant: ",num2str(tau_open)));
+annotation('textbox',[0.2,0.22,0.1,0.1],'String',strcat("Settling Time: ",num2str(settling_time_open)));
+%% 1.5
+[y_t,y_v,u_t,u_v] = run_sim('Lab_1_5.slx');
+tau_y_closed = dc_gain_closed*0.63 - dc_gain_closed/2;
+set_time_low_closed = dc_gain_closed/2 * 0.98;
+set_time_high_closed = dc_gain_closed/2 * 1.02;
+
+% From graph
+tau_closed = 0.00251806053;
+settling_time_closed = 0.01165713981;
+
+figure(6);
+plot([u_t(:), y_t(:)], [u_v(:), y_v(:)], '-');
+hold on
+yline(tau_y_closed,'c-');
+hold on
+yline(set_time_low_closed,'g-');
+hold on
+yline(set_time_high_closed,'g-');
+hold on
+plot(1+tau_closed,tau_y_closed,'ro');
+hold on
+plot(1+settling_time_closed, set_time_low_closed,'bo');
+
+legend('Input Signal', 'Output Signal');
+xlabel('Time')
+
+title('Settling Time and Time Constant for Closed-loop Step-Response')
+annotation('textbox',[0.2,0.15,0.1,0.1],'String',strcat("Time Constant: ",num2str(tau_closed)));
+annotation('textbox',[0.2,0.22,0.1,0.1],'String',strcat("Settling Time: ",num2str(settling_time_closed)));
+%% 1.6
+[y_t,y_v,u_t,u_v] = run_sim('Lab_1_6.slx');
+
+% Markers at Peaks
+[maxy,maxind]=max(y_v);
+[miny,minind]=min(y_v);
+minx = y_t(minind);
+maxx = y_t(maxind);
+px = [minx, maxx];
+py = [miny, maxy];
+
+figure(7);
+plot([u_t(:), y_t(:)], [u_v(:), y_v(:)], '-');
+hold on
+plot(px,py,'go');
+legend('Input Signal', 'Output Signal', 'Peaks');
+xlabel('Time')
+
+title('Gain for Saturation/Clipping')
+annotation('textbox',[0.2,0.15,0.1,0.1],'String',strcat("Kp: ",num2str(20.5)));
+%%
+function [y_t, y_v, u_t, u_v] = run_sim(slx_name)
+    % Run the simulink diagram!
+    sim(slx_name);
+    % Once the simulink diagram executes, it generates a variable "logsout". We
+    % can inspect this variable to pull out the signals we have recorded!
+    % Pull out the Output (y) and Input (u) signals.
+    y_t = logsout.getElement('y').Values.Time;      % Get the Time Points for the output signal 'y'
+    y_v = logsout.getElement('y').Values.Data;      % Get the values for the output signal 'y'
+    u_t = logsout.getElement('u').Values.Time;      % Get the time points for the input signal 'u'
+    u_v = logsout.getElement('u').Values.Data;      % Get the values for the input signal 'u'
+end
